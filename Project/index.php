5e881ae6382ad49849fdfce23f2c3224091d67b6
Abdullah <itsabdullah14311211@gmail.com>
2024-07-03 17:00:15 +0500

    Add files via upload

---
+++ Project/index.php
@@ -0,0 +1,14 @@
+<?php
+
+// $arr = array("hello", "world", "car");
+// $arr[3] = "aaa";
+
+$aae = array(
+     'name' => 'abdullah',
+     'class'=> '9th e',
+);
+echo "<pre>";
+print_r($aae); 
+echo "</pre>";
+
+?>
